f9042e066f9dfac731742b875f4af37447d16919
Ali Sadri <asadri721@gmail.com>
2021-11-24 11:55:53 +0330

    Complate widgets section

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,9 +1,35 @@
+import {
+  TwitterTweetEmbed,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+} from 'react-twitter-embed';
+import SearchIcon from '@mui/icons-material/Search';
 import './Widgets.css';
 
 const Widgets = () => {
   return (
     <div className='widgets'>
-      <h2>Widgets</h2>
+      <div className='widgets__input'>
+        <SearchIcon classname='widgets__searchIcon' />
+        <input type='text' placeholder='Search twitter' />
+      </div>
+
+      <div className='widgets__widgetContainer'>
+        <h2>What's Happening</h2>
+
+        <TwitterTweetEmbed tweetId='858551177860055040' />
+
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='clever'
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={'https://facebook.com/#'}
+          options={{ text: 'some text', via: 'dsadsa' }}
+        />
+      </div>
     </div>
   );
 };
