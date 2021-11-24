a1226e398e0530e0c22c35484c694ee8cd661ec4
Ali Sadri <asadri721@gmail.com>
2021-11-24 13:40:25 +0330

    Add real-time database with firebase

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,23 +1,34 @@
+import { useEffect, useState } from 'react';
 import TweetBox from './TweetBox';
 import Post from './Post';
 import './Feed.css';
-import sample_avatar from '../../assets/sample_avatar.png';
+import db from '../../firebase';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
   return (
     <div className='feed'>
       <div className='feed__header'>
         <h2>Feed</h2>
       </div>
       <TweetBox />
-      <Post
-        displayName='ali sadri'
-        username='a.sadri'
-        verified={true}
-        text='thats coool'
-        image='https://media2.giphy.com/media/pW9HxoGcgVeP9eeory/giphy.gif?cid=ecf05e47bb71077a5af6ee9ede51e192740fd8aeadfa318a&rid=giphy.gif&ct=g'
-        avatar={sample_avatar}
-      />
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          image={post.image}
+          avatar={post.avatar}
+        />
+      ))}
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBaA6EDQ3ERVH86P_klj7H3NsLh7NPCjLc',
